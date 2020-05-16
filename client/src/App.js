b067f2193fcea43ec97835c8d677bc5b66eed914
Jenn Soderborg <jsoderborg7@gmail.com>
2020-05-16 11:38:39 -0600

    working on doughnut chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
 
-import Header from './components/Header'
 import PersonalDashboard from './components/Dashboards/PersonalDashboard'
 import './scss/index.scss';
 
@@ -15,7 +14,6 @@ function App() {
       <div className='routes'>
         <Route exact path='/' component={PersonalDashboard}/>
       </div>
-      <Header />
       <Footer />
     </div>
   );
--- client/src/components/Dashboards/PersonalDashboard.js
+++ client/src/components/Dashboards/PersonalDashboard.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import '../../scss/personalDashboard.scss'
+import {PieChart} from 'react-minimal-pie-chart'
 
 const PersonalDashboard = () =>{
+
   return(
     <div className='personal-dashboard-container'>
-      <h2>My Objectives</h2>
-      <h2>My Tactics</h2>
-      <h2>My Committee Objectives</h2>
+      <div className='container'>
+        <h2>My Objectives</h2>
+        <hr/>
+        <div className='status'>
+        <PieChart
+          className='PieChart'
+          data={[
+            { title: 'One', value: 10, color: '#E38627' },
+            { title: 'Two', value: 15, color: '#C13C37' },
+            { title: 'Three', value: 20, color: '#6A2135' },
+          ]}
+          lineWidth={55}
+        />
+        </div>
+      </div>
+      <div className='container'>
+        <h2>My Tactics</h2>
+        <hr/>
+        <div className='status'>
+          
+        </div>
+      </div>
+      <div className='container'>
+        <h2>My Committee Objectives</h2>
+        <hr/>
+        <div className='status'>
+          
+        </div>
+      </div>
     </div>
   )
 }
